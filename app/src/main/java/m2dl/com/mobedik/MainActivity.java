ec777fcded0505eab8d2ba55d72e0e9d981d7271
TimotheeJammot <tim.jammot@yahoo.fr>
2017-03-15 22:52:38 +0100

    occupation changes

--- app/src/main/java/m2dl/com/mobedik/MainActivity.java
+++ app/src/main/java/m2dl/com/mobedik/MainActivity.java
@@ -1,9 +1,11 @@
 package m2dl.com.mobedik;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.occupation:
+                startActivity(new Intent(this, OccupationActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
